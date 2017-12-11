289222b1169f59170415be38996ec78666cb6541
Arnold <amlestin@gmail.com>
2017-12-11 12:10:40 -0500

    Changed variable names to be more clear.

--- find_class_sizes.R
+++ find_class_sizes.R
@@ -4,9 +4,9 @@ c <- read.csv(INPUT_FILENAME)
 
 all_titles <- as.character(c$Course.Title)
 unique_titles <- unique(all_titles)
-counts <- list(unique_titles)
+course_sizes <- list(unique_titles)
 
 for (title in 1:length(unique_titles)){
   cur_title <- unique_titles[title]
-  counts[[cur_title]] <- length(which(all_titles==cur_title))  
+  course_sizes[[cur_title]] <- length(which(all_titles==cur_title))  
 }

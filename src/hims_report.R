ebe69cdf322bf32529b24b22d6c188ae5004ea06
Arnold Lestin <amlestin@gmail.com>
2019-12-18 16:51:14 -0500

    commented script

--- src/hims_report.R
+++ src/hims_report.R
@@ -16,6 +16,7 @@ response.cols <- grep("X[0-9]", colnames(dist))
 # remove comments column
 response.cols <- response.cols[-length(response.cols)]
 
+# collect the questions and their averages
 q.a <- c()
 for (col.num in 1:length(response.cols)) {
   question.col <- as.vector(dist[, response.cols[col.num]])
@@ -28,20 +29,26 @@ for (col.num in 1:length(response.cols)) {
   q.a <-
     rbind(q.a, c(dist[1, response.cols[col.num]], question.average, length(question.col)))
 }
-
 names(q.a) <- NULL
+
+# add column names
 q.a <-
   rbind(c("", "", ""), c("Question", "Average", "# Responses"), q.a)
 q.a <- rbind(c("HIMS Course Evaluations", "", ""), q.a)
 
+# extract and format comments
 comments <-  dist[3:nrow(dist), "X13"]
 comments <- comments[-which(comments == "")]
 comments <- paste(">>>", comments)
 comments <- cbind(comments, rep("", length(comments)))
 comments <- cbind(comments, rep("", length(comments)))
+
+# append comments to report
 q.a <-
   rbind(q.a, c("", "", ""), c("Comments", "", ""), c("", "", ""), comments)
 
+
+# define formatting of each report section
 headerStyle <-
   createStyle(fontSize = 20,
               fontColour = "#333333",
@@ -81,28 +88,36 @@ commentHeaderStyle <-
     borderStyle = "thick"
   )
 
+# set the report column widths
 setColWidths(wb, 1, cols = 1, widths = 90)
 setColWidths(wb, 1, cols = 2, widths = 13)
 setColWidths(wb, 1, cols = 3, widths = 13)
 
+# format the report header text
 addStyle(wb, 1, headerStyle, cols = 1, rows = 1)
 
+# merge the cells of the report header
+mergeCells(wb, 1, cols = 1:3, rows = 1)
+
+# format the comments header
 addStyle(wb, 1, commentHeaderStyle, cols = 1, rows = 17)
 addStyle(wb, 1, commentHeaderStyle, cols = 2, rows = 17)
 addStyle(wb, 1, commentHeaderStyle, cols = 3, rows = 17)
 
-mergeCells(wb, 1, cols = 1:3, rows = 1)
-
+# apply formatting to the table
 Map(function(i) {
   addStyle(wb, 1, tableStyle, cols = 1, rows = i)
   addStyle(wb, 1, tableStyle, cols = 2, rows = i)
   addStyle(wb, 1, tableStyle, cols = 3, rows = i)
 }, 3:15)
+
+# apply formatting to all comments
 Map(function(r) {
   addStyle(wb, 1, textStyle, cols = 1, rows = r)
   mergeCells(wb, 1, cols = 1:3, rows = r)
 }, 18:(14 + length(comments)))
 
+# apply tableHeaderStyle to each column of the table header
 addStyle(wb, 1, tableHeaderStyle, cols = 1, rows = 3)
 addStyle(wb, 1, tableHeaderStyle, cols = 2, rows = 3)
 addStyle(wb, 1, tableHeaderStyle, cols = 3, rows = 3)
@@ -113,10 +128,12 @@ pageSetup(wb,
           fitToWidth = TRUE,
           fitToHeight = FALSE)
 
+# calculate the vertical heights of the comments
 comments <- comments[-which(comments == "")]
 heights <- strwidth(comments, units = "in", font = 1) * 10 / 1.43
 heights <- heights * 1.2
 setRowHeights(wb, 1, rows = 19:nrow(q.a), heights)
 
+# write to the workbook then write the xlsx file to disk
 writeData(wb, 1, q.a, colNames = FALSE)
 saveWorkbook(wb, "HIMS-report.xlsx", overwrite = TRUE)

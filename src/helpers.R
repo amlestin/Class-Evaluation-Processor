fc3e7a08b786e41de4a06474d148938249a12aba
Arnold Lestin <amlestin@gmail.com>
2020-02-24 11:09:34 -0500

    Moved function out of courseReport

--- src/helpers.R
+++ src/helpers.R
@@ -1,3 +1,47 @@
+evals.to.average <- function(reviews) {
+  # counts frequencies of each possible review response
+  if (any(reviews == "Excellent") |
+      any(reviews == "Very Good") |
+      any(reviews == "Good") |
+      any(reviews == "Fair") | any(reviews == "Poor")) {
+    ecount <- length(which(reviews == "Excellent"))
+    
+    vgcount <- length(which(reviews == "Very Good"))
+    
+    gcount <- length(which(reviews == "Good"))
+    
+    fcount <- length(which(reviews == "Fair"))
+    
+    pcount <- length(which(reviews == "Poor"))
+  } else {
+    ecount <- length(which(reviews == "5"))
+    
+    vgcount <- length(which(reviews == "4"))
+    
+    gcount <- length(which(reviews == "3"))
+    
+    fcount <- length(which(reviews == "2"))
+    
+    pcount <- length(which(reviews == "1"))
+  }
+  
+  # vector of all rating counts
+  freqs <- c(ecount, vgcount, gcount, fcount, pcount)
+  
+  # sums frequencies of all reviews in the section, i.e., total number of ratings the professor received
+  num.ratings <- sum(freqs)
+  
+  # calculates a weighted average
+  ratings.prod <-
+    (5 * ecount + 4 * vgcount + 3 * gcount + 2 * fcount + 1 * pcount)
+  
+  average <- ratings.prod / num.ratings
+  average <- round(average, digits = 2)
+  average <- sprintf("%0.2f", average)
+  
+  return (average)
+}
+
 get_worksheet_entries <- function(wb, sheet) {
   #  https://github.com/awalker89/openxlsx/pull/382/commits/7e9cfd09934ac61491b71e91785c415a22b3dc31
   # get worksheet data
